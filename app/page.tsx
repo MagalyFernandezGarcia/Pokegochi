d5eade47e2ab4b56fd349e23e67a2e7914465663
Magaly <fernandezgarciamagaly@gmail.com>
2025-05-05 16:15:57 +0200

    adding prisma + create schema

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,15 @@ import SignUpForm from "./SignUpForm";
 
 export default async function Home() {
   const session = await auth()
-  console.log(session);
+ 
   return (
-    <div>
-      home
-    </div>
+    <section className="flex flex-col items-center justify-center h-full gap-8">
+      <div>
+        New save
+      </div>
+      <div>
+        Save 1
+      </div>
+    </section>
   );
 }
