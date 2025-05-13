c58b5fe63b547587c967dd61f81366ec905a15e1
Magaly <fernandezgarciamagaly@gmail.com>
2025-05-13 09:27:37 +0200

    Modify save type so a user can have multiple save

--- features/createSave.ts
+++ features/createSave.ts
@@ -1,17 +1,25 @@
-"use server"
+"use server";
 
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
+export async function createSave(mail: string, name: string) {
+  console.log("function");
 
-export async function createSave( mail : string, name : string) {
-    console.log("function");
-    
-     await prisma.save.create({data: {
-        userMail: mail,
-        userName: name,
-        playerLevel: 1,
-        money: 0
-    }})
-    revalidatePath("/")
+ await prisma.save.upsert({
+  where: { userMail: mail },
+  update: {
+    userName: {
+      push: name,
+    },
+  },
+  create: {
+    userMail: mail,
+    userName: [name],
+    playerLevel: 1,
+    money: 0,
+    playerXp: 0,
+  },
+});
+  revalidatePath("/");
 }
