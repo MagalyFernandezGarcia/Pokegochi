4ed8e188a1ff8adace073bdb38d735f90e098a03
Magaly <fernandezgarciamagaly@gmail.com>
2025-05-14 11:55:06 +0200

    prisma changes

--- features/createSave.ts
+++ features/createSave.ts
@@ -2,24 +2,47 @@
 
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { findUser } from "./findUser";
 
-export async function createSave(mail: string, name: string) {
-  console.log("function");
+export async function createSave( name: string) {
+  const user = await findUser();
+  const mail = user.userMail;
 
- await prisma.save.upsert({
-  where: { userMail: mail },
-  update: {
-    userName: {
-      push: name,
+  const dubble = await prisma.userName.findFirst({
+    where: {
+      name: name,
+      save: {
+        userMail: mail,
+      },
     },
-  },
-  create: {
-    userMail: mail,
-    userName: [name],
-    playerLevel: 1,
-    money: 0,
-    playerXp: 0,
-  },
-});
+  });
+
+ if(!dubble){await prisma.save.upsert({
+    where: {
+      userMail: mail,
+    },
+    update: {
+      userNames: {
+        create: {
+          name: name,
+        },
+      },
+    },
+    create: {
+      userMail: mail,
+      playerLevel: 0,
+      playerXp: 0,
+      money: 0,
+      userNames: {
+        create: {
+          name: name,
+        },
+      },
+    },
+  });}
+  
+
+  
+
   revalidatePath("/");
 }
