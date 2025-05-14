8d2eef98951534af9c54826b127dd7fded55279b
Magaly <fernandezgarciamagaly@gmail.com>
2025-05-14 11:55:19 +0200

    prisma chnage bis

---
+++ features/findUser.ts
@@ -0,0 +1,23 @@
+"use server"
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+
+
+
+export async function findUser (){
+    const session = await auth()
+     const userMail = session?.user?.email;
+
+  if (!userMail) {
+    throw new Error("Missing userId");
+  }
+  const user = await prisma.save.findUnique({
+    where: {
+      userMail: userMail,
+    },
+  })
+  if (!user) {
+    throw new Error("User not found");
+  }
+  return user
+}
