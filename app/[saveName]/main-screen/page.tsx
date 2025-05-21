01372d484042ceec86da6a0ccde9c63d6b86544d
Magaly <fernandezgarciamagaly@gmail.com>
2025-05-21 11:28:57 +0200

    mood indicator

--- app/[saveName]/main-screen/page.tsx
+++ app/[saveName]/main-screen/page.tsx
@@ -3,6 +3,7 @@ import { getPokemonFromSave } from "@/features/getPokemonFromSave";
 import { getPokemon } from "@/features/getPokemons";
 import { getSave } from "@/features/getSave";
 import { Pokemon } from "@/types/pokemon";
+import { Angry, Annoyed, Frown, Smile } from "lucide-react";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
@@ -21,27 +22,44 @@ export default async function MainScreen({
     pkmName: currentPkmn.name,
   });
 
+  const happyMeter = () => {
+    const size = 40;
+    const totalStats =
+      currentPkmn.happiness +
+      currentPkmn.hunger +
+      currentPkmn.pv +
+      currentPkmn.cleanliness;
+    const average = totalStats / 4;
+    if (average < 10) return <Angry size={size} color="red" />;
+    if (average < 30) return <Frown size={size} color="orange" />;
+    if (average < 70) return <Annoyed size={size} color="yellow" />;
+    return <Smile size={size} color="green" />;
+  };
+
   return (
-    <main className="flex flex-col  items-center ">
-      <section className="flex gap-4">
+    <main className="flex flex-col  items-center h-full ">
+      <section className="flex gap-4 self-start pt-4 pl-4">
         <div>Player : {params.saveName}</div>{" "}
         <div> Level : {save?.[0].playerLevel}</div>
       </section>
 
-      <section className="flex flex-col justify-center items-center h-full  ">
-        <Image
-          src={lastPkmn.sprites.front_default}
-          alt={lastPkmn.name}
-          width={200}
-          height={200}
-        />
+      <section className="flex flex-col justify-center items-center h-1/3 ">
         <div className="flex gap-4">
           <div>{lastPkmn.name}</div>
           <div>Level : {currentPkmn.level}</div>
         </div>
+        <div className="flex gap-4 items-center">
+          <Image
+            src={lastPkmn.sprites.front_default}
+            alt={lastPkmn.name}
+            width={200}
+            height={200}
+          />
+          {happyMeter()}
+        </div>
       </section>
       <section className="flex gap-4">
-        <ProgressBar current={50} max={lastPkmn.stats[0].base_stat}>
+        <ProgressBar current={currentPkmn.pv} max={lastPkmn.stats[0].base_stat}>
           HP
         </ProgressBar>
         <ProgressBar current={currentPkmn.happiness} max={100}>
